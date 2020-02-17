a12a8b25359a369738a52947ee663a562809a619
Yurii Tkachuk <papirus.borzna@gmail.com>
2020-02-17 18:46:24 +0200

    stage8(correction magic numbers)

--- src/games/progression-games.js
+++ src/games/progression-games.js
@@ -3,7 +3,8 @@ import { GameEngine } from '..';
 const Task = 'What number is missing in the progression?';
 const CheckAnswer = (arg1, arg2) => arg1 === +arg2;
 const expression = (StartNum, RandomNum) => {
-  const index = (RandomNum > 10) ? Math.floor(RandomNum / 10) : RandomNum;
+  const ReductoinCoefficient = 10;
+  const index = (RandomNum > 10) ? Math.floor(RandomNum / ReductoinCoefficient) : RandomNum;
   let c = StartNum;
   let string = c;
   let NumForCheckAnswer;
