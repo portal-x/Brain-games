6460266570db26a737aaaafc5c9903394180d3ed
Yurii Tkachuk <papirus.borzna@gmail.com>
2020-03-30 14:45:22 +0300

    finish

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,15 +4,15 @@ import randomNum from '../utils';
 const task = 'What number is missing in the progression?';
 const minNumber = 0;
 const maxNumber = 100;
-const progressionLenght = 10;
+const progressionLength = 10;
 const rangeDiffValue = 10;
 
 const generateGameData = () => {
   const start = randomNum(minNumber, maxNumber);
   const diff = randomNum(1, rangeDiffValue);
-  const index = randomNum(0, (progressionLenght - 1));
+  const index = randomNum(0, (progressionLength - 1));
   let question = '';
-  for (let count = 0; count < progressionLenght; count += 1) {
+  for (let count = 0; count < progressionLength; count += 1) {
     if (count === index) {
       question = `${question} ..`;
     } else {
